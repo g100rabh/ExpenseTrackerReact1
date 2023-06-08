f6162a205f80c10531a1d6e9f8b6d92c8aa9daa3
g100rabh <gsaurabh909@gmai.com>
2023-06-08 11:03:31 +0530

    Expense1

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,24 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>
+                <h2>Expense Items</h2>
+            </div>
+            <div>
+                <span>Food</span>
+                <span> Rs 10</span>
+            </div>
+            <div>
+                <span>Petrol</span>
+                <span> Rs 100</span>
+            </div>
+            <div>
+                <span>Movies</span>
+                <span> Rs 200</span>
+            </div>
+        </div>
+    
+    );
+}
+
+export default ExpenseItem;
