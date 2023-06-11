554e11b7008757c4444fdbfdf604abc7660d9c4e
g100rabh <gsaurabh909@gmai.com>
2023-06-11 22:12:41 +0530

    Expense9 Submitting the forms by myself

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -9,25 +9,61 @@ const ExpenseForm = () => {
     const [enteredDate, setEnteredDate] = useState('');
     const [enteredlocation, setEnteredLocation] = useState('');
 
+    // const[userInput,setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: '',
+    //     enteredlocation: ''
+    // });
+
     const inputChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value
+        // });
+        // setUserInput((prevState) => {
+        //     return{...prevState, enteredTitle: event.target.value
+        //     };
+        // });
     }
 
     const locationChangeHandler = (event) => {
         setEnteredLocation(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredlocation: event.target.value
+        // });
     }
 
     const amountChangeHandler = (event) => {
         setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value
+        // });
     }
 
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value
+        // });
     }
+    // console.log(enteredTitle)
+    const form = document.querySelector('.form');
+    const clickSubmitHandler = (event) =>{
+        event.preventDefault();
+        console.log(enteredTitle, enteredDate, enteredAmount, enteredlocation);
+        form.reset();
+        
+    }
+    
 
     return (
-        <form>
-            <div className='expense-input'>
+        <form className='form' >
+            <div className='expense-input' >
                 <div className='expense-input'>
                     <label>Title:</label>
                     <input type="text" onChange={inputChangeHandler}/>
@@ -46,7 +82,7 @@ const ExpenseForm = () => {
                 </div>
             </div>
             <div className='expense-input'>
-                <button type='submit'>Add Expense</button>
+                <button type='submit' onClick={clickSubmitHandler}>Add Expense</button>
             </div>
         </form>
        
