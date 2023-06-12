290a8eb4f1be116a63336d08009f8958d4b10022
g100rabh <gsaurabh909@gmai.com>
2023-06-12 15:09:59 +0530

    Expense10 Making the expense form input visible on screen

--- src/App.js
+++ src/App.js
@@ -1,48 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Expenses from './components/Expenses/Expenses';
+import NewExpenses from './components/NewExpenses/NewExpenses';
 
 // function App() {
 const App = () => {  // Arrow funtion
-  const expenses = [
+  const initialExpenses = [
     {
       id: 'e1',
       title: 'des1',
-      amount: 40,
+      amount: "40",
       date: new Date(2023, 5, 3),
       location: 'shahdol'
     },
     {
       id: 'e2',
       title: 'des2',
-      amount: 90,
+      amount: "90",
       date: new Date(2023, 2, 3),
       location: 'bhopal'
     },
     {
       id: 'e3',
       title: 'des3',
-      amount: 200,
+      amount: "200",
       date: new Date(2023, 1, 3),
       location: 'indore'
     },
     {
       id: 'e4',
       title: 'des4',
-      amount: 10,
+      amount: "10",
       date: new Date(2023, 4, 3),
       location: 'shahdol'
-    },
+    }
   ];
-
-  // return React.createElement(
+   // return React.createElement(
   //   'div',
   //   {},
   //   React.createElement(Expenses, {items: expenses})
   // )
+  const [expenses, setExpenses] = useState(initialExpenses);
+
+  const addExpenseHandler = (expense) => {
+    // console.log(expense);
+    // expenses.push(expense);
+    // console.log(expenses)
+    setExpenses((initialExpenses) => {
+      return [...initialExpenses, expense]
+    })
+  }
+  // console.log(expenses, initialExpenses)
+  
+ 
 
 
   return (
     <div>
+      <NewExpenses onAddExpense={addExpenseHandler}/>
       <Expenses items = {expenses}/>
     </div>
   );
